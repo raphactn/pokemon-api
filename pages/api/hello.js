a4f5aa485c62a08ac45af888c0dccac413ba8779
raphael <raphael@leeksolucoes.com.br>
2022-07-14 15:03:03 -0300

    first Commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const api = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/'
+});
+
+export default api;
